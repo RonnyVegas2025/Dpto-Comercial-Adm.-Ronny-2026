00e7b79a9089e95f79f76a4d8f10ee689cc017ad
RonnyVegas2025 <ronnyizidorio46@gmail.com>
2026-05-21 09:07:29 -0300

    Update page.jsx

--- app/evolucao/page.jsx
+++ app/evolucao/page.jsx
@@ -751,7 +751,7 @@ export default function Evolucao() {
         supabase.from('empresas').select(`id, produto_id, nome, cnpj, cidade, estado, categoria, produto_contratado, potencial_movimentacao, peso_categoria,
           data_cadastro, pct_principal, pct_agregado_1, pct_agregado_2,
          consultor_principal:consultor_principal_id (id, nome, setor, equipe, gestor, diretor, diretor_id, tipo, diretorObj:diretor_id(id,nome)),
-          consultor_agregado:consultor_agregado_id (id, nome, setor, equipe, gestor),
+          consultor_agregado:consultor_agregado_id (id, nome, setor, equipe, gestor, diretor, diretor_id, diretorObj:diretor_id(id,nome)),
           consultor_agregado_2:consultor_agregado_2_id (id, nome, setor, equipe, gestor)`)
           .eq('ativo', true),
         supabase.from('liberacoes').select('produto_id, competencia, total_liberado').order('competencia'),
@@ -790,7 +790,7 @@ export default function Evolucao() {
           .select(`id, produto_id, nome, cnpj, cidade, estado, categoria, produto_contratado, potencial_movimentacao, peso_categoria,
             data_cadastro, pct_principal, pct_agregado_1, pct_agregado_2,
             consultor_principal:consultor_principal_id (id, nome, setor, equipe, gestor, diretor, diretor_id, tipo, diretorObj:diretor_id(id,nome)),
-            consultor_agregado:consultor_agregado_id (id, nome, setor, equipe, gestor),
+            consultor_agregado:consultor_agregado_id (id, nome, setor, equipe, gestor, diretor, diretor_id, diretorObj:diretor_id(id,nome)),
             consultor_agregado_2:consultor_agregado_2_id (id, nome, setor, equipe, gestor)`)
           .eq('ativo', true),
         supabase.from('liberacoes').select('produto_id, competencia, total_liberado').order('competencia'),
